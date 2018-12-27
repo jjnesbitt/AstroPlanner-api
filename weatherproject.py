769a5221d008773895f19e693d904e26a39cddf9
Sam luyk <samluyk@users.noreply.github.com>
2018-12-26 19:31:09 -0500

    Add files via upload
    oh boy I dont know what I'm doing

---
+++ weatherproject.py
@@ -0,0 +1,14 @@
+WeatherProject.py
+
+#Sam Luyk & Jake Nesbitt, a pythond project to determine if it's worth going out
+#to shoot star photos.
+
+#Links:
+#https://forecast.weather.gov/MapClick.php?lat=43.33441000000005&lon=-73.67806999999993
+#https://www.moongiant.com/calendar/
+#http://www.cleardarksky.com/c/GlnsFllsNYkey.html?1
+#https://www.aerisweather.com/support/docs/api/reference/endpoints/sunmoon-moonphases/
+#https://www.weather.gov/documentation/services-web-api
+#http://bazaar.launchpad.net/~keturn/py-moon-phase/trunk/view/head:/moon.py
+#https://api.weather.gov/gridpoints/ALY/57,89/forecast
+#https://api.weather.gov/gridpoints/ALY/58,90/forecast
