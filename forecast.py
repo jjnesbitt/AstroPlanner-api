76280f1479b0182176527a7cd06ce2bd3cbb9a26
Yakob <jjnesbitt2@gmail.com>
2019-01-02 22:18:39 -0500

    fix hard coding in moonInfo func

--- forecast.py
+++ forecast.py
@@ -11,17 +11,17 @@ GLENS_FALLS_LAT = 43.3095
 GLENS_FALLS_LONG = -73.6440
 GLENS_FALLS_LAT_TUPLE = (43, 30, 95)
 GLENS_FALLS_LONG_TUPLE = (-73, 64, 40)
-BASE_API_PATH = 'https://api.darksky.net/forecast/' + DARK_SKY_SECRET
+DARK_SKY_API_PATH = 'https://api.darksky.net/forecast/' + DARK_SKY_SECRET
 GLENS_FALLS_PATH = '/' + str(GLENS_FALLS_LAT) + ',' + str(GLENS_FALLS_LONG)
 #######################
 
 
 def raw_forecast(path=GLENS_FALLS_PATH):
     res = None
-    params = {'extend': 'hourly'}
+    params = {'extend': 'hourly', 'exclude': ['minutely']}
     headers = {'content-encoding': 'gzip'}
     try:
-        res = requests.get(BASE_API_PATH + path, params=params, headers=headers)
+        res = requests.get(DARK_SKY_API_PATH + path, params=params, headers=headers)
         return json.loads(res.text)
     except:
         print("Error retrieving forcast")
@@ -35,17 +35,23 @@ def forecast(path=GLENS_FALLS_PATH):
         return None
 
     for day in res['daily']['data']:
-        darkHours = list(filter(lambda x: (x['time'] < day['sunriseTime'] or x['time'] > day['sunsetTime'])
-            and x['time'] > day['time']
-            and x['time'] < day['time'] + 86400, res['hourly']['data']))
-        day['darkHours'] = darkHours
+        day['hours'] = list(filter(lambda x: x['time'] > day['time'] and x['time'] < day['time'] + 86400, res['hourly']['data']))
+        for hour in day['hours']:
+            hour['dark'] = (hour['time'] < (day['sunriseTime'] - 10800) or hour['time'] > (day['sunsetTime'] + 10800))
 
     return res
 
-def moonInfo(time=datetime.now().timestamp(), lat=GLENS_FALLS_LAT_TUPLE, long=GLENS_FALLS_LONG_TUPLE):
+
+def moonInfo(time=datetime.now().timestamp(), lat=GLENS_FALLS_LAT, lon=GLENS_FALLS_LONG):
+    lat_string_pair = "{:.4f}".format(lat).split('.')
+    lon_string_pair = "{:.4f}".format(lon).split('.')
+
+    lat_tuple = (int(lat_string_pair[0]), int(lat_string_pair[1][:2]), int(lat_string_pair[1][2:]))
+    lon_tuple = (int(lon_string_pair[0]), int(lon_string_pair[1][:2]), int(lon_string_pair[1][2:]))
+
     time = datetime.fromtimestamp(time)
-    Moon = pylunar.MoonInfo(lat, long)
+    Moon = pylunar.MoonInfo(lat_tuple, lon_tuple)
     Moon.update(time.utctimetuple()[:6])
-    
+
     rise_set_times = Moon.rise_set_times('US/Eastern')
     return {x[0]: datetime(*x[1], 0, tzlocal.get_localzone()).timestamp() for x in rise_set_times}
--- main.py
+++ main.py
@@ -10,18 +10,13 @@ import forecast as weatherForcast
 ####################
 
 forecast = weatherForcast.forecast()
-forecastDays = forecast['daily']['data']
-# moonInfo = [weatherForcast.moonInfo(time=x['time']) for x in forecastDays]
-
-for day in forecastDays:
+for day in forecast['daily']['data']:
     day['moonInfo'] = weatherForcast.moonInfo(time=day['sunriseTime'])
-    # day['darkHoursNoMoon'] = list(filter(lambda x: (x['time'] < day['moonInfo']['rise'] or x['time'] > day['moonInfo']['set']), day['darkHours']))
     
-    for hour in day['darkHours']:
+    for hour in day['hours']:
         hour['moonVisible'] = (hour['time'] >= day['moonInfo']['rise'] and hour['time'] <= day['moonInfo']['set'])
     
-    print((len(day['darkHours']), len(day['darkHoursNoMoon'])))
-
-# print(forecastDays)
+    # print(len(day['hours']))
+    
 with open('forecast.json', 'w') as outfile:
-    json.dump(forecastDays, outfile, indent=4)
+    json.dump(forecast, outfile, indent=4)
