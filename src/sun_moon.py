38ae6f89fab09e43d423a84154c1815d8406becc
Jacob Nesbitt <nesbitjj@clarkson.edu>
2019-01-11 14:03:02 -0500

    Commented new way for finding body angles

--- src/sun_moon.py
+++ src/sun_moon.py
@@ -1,6 +1,7 @@
 from skyfield.toposlib import Topos
 from skyfield.api import Loader
-from skyfield.almanac import fraction_illuminated
+import skyfield.almanac as almanac
+from skyfield.elementslib import osculating_elements_of
 from datetime import datetime
 import pytz
 
@@ -16,6 +17,25 @@ lat, lng should be float values
 time should be a unix timestamp
 '''
 
+def body_above_angle(ephemeris, body, angle, topos):
+
+    """Build a function of time that returns whether the specified body is above the desired angle.
+
+    The function that this returns will expect a single argument that is
+    a :class:`~skyfield.timelib.Time` and will return ``True`` if the
+    sun is up, else ``False``.
+
+    """
+    body_to_observe = ephemeris[body]
+    topos_at = (earth + topos).at
+
+    def is_body_up_at(t):
+        """Return `True` if the body has risen by time `t`."""
+        # t._nutation_angles = iau2000b(t.tt)
+        return topos_at(t).observe(body_to_observe).apparent().altaz()[0].degrees > angle
+
+    is_body_up_at.rough_period = 0.5  # twice a day
+    return is_body_up_at
 
 def moon_info(lat, lng, time=None):
     if (time == None):
@@ -29,7 +49,7 @@ def moon_info(lat, lng, time=None):
     moon_loc = loc.at(time).observe(moon).apparent()
     moon_alt, _, _ = moon_loc.altaz()
 
-    frac = fraction_illuminated(planets, 'moon', time)
+    frac = almanac.fraction_illuminated(planets, 'moon', time)
     return {
         'pos': moon_alt.degrees,
         'above_horizon': bool(moon_alt.degrees > 0),
@@ -37,15 +57,22 @@ def moon_info(lat, lng, time=None):
     }
 
 
-def sun_info(lat, lng, time=None):
-    if (time == None):
-        time = ts.now().astimezone(pytz.utc)
-    else:
-        time = datetime.fromtimestamp(time).astimezone(pytz.utc)
+def sun_info(lat, lng, timestamp=None):
+    if (timestamp == None):
+        timestamp = datetime.now().timestamp()
+        # time = ts.now().astimezone(pytz.utc)
+
+    time = datetime.fromtimestamp(timestamp).astimezone(pytz.utc)
+    # time2 = datetime.fromtimestamp(timestamp + 500000).astimezone(pytz.utc)
     time = ts.utc(time)
+    # time2 = ts.utc(time2)
 
     sun = planets['sun']
-    loc = earth + Topos(latitude_degrees=lat, longitude_degrees=lng)
+    earth_loc = Topos(latitude_degrees=lat, longitude_degrees=lng)
+    loc = earth + earth_loc
+    # t, y = almanac.find_discrete(time, time2, body_above_angle(planets, 'sun', -18, earth_loc))
+    # print([x.utc_datetime().isoformat() for x in t], y)
+
     sun_loc = loc.at(time).observe(sun).apparent()
     sun_alt, _, _ = sun_loc.altaz()
 
--- src/test.py
+++ src/test.py
@@ -12,18 +12,18 @@ import main
 GLENS_FALLS_LAT = 43.3095
 GLENS_FALLS_LONG = -73.6440
 
-start = time.time()
-res = forecast.forecast(lat=12.4444, lng=21.3333)
-end = time.time()
-print("runtime of forecast", end - start)
+# start = time.time()
+# res = forecast.forecast(lat=12.4444, lng=21.3333)
+# end = time.time()
+# print("runtime of forecast", end - start)
+
+# start = time.time()
+# res = forecast.raw_forecast(lat=12.4444, lng=21.3333)
+# end = time.time()
+# print("runtime of raw_forecast", end - start)
 
 start = time.time()
-res = forecast.raw_forecast(lat=12.4444, lng=21.3333)
-end = time.time()
-print("runtime of raw_forecast", end - start)
-
-start = time.time()
-res = sun_moon.sun_moon_info(lat=12.4444, lng=21.3333)
+res = sun_moon.sun_moon_info(lat=GLENS_FALLS_LAT, lng=GLENS_FALLS_LONG)
 end = time.time()
 print("runtime of sun_moon_info", end - start)
 
