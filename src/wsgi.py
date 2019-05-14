4ad9f60ff4b85c73e6ce9244837914375ebebddf
Yakob <jjnesbitt2@gmail.com>
2019-05-14 14:46:07 -0400

    add fixes for pipenv and CORS

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,5 +3,12 @@ import main as forecast
 
 def application(environ, start_response):
     params = urllib.parse.parse_qs(environ['QUERY_STRING'])
-    start_response('200 OK', [('Content-Type', 'application/json')])
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'application/json'),
+        ('Access-Control-Allow-Origin', 'localhost:3000'),
+        ('Access-Control-Allow-Credentials', 'true'),
+    ]
+
+    start_response(status, headers)
     return forecast.main(params).encode('utf-8')
